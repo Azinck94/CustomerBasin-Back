081200b2473a5ea3951e11976dff71e4135eb80a
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-25 10:49:40 -0400

    scriptscustomers

--- scripts/create-customer.sh
+++ scripts/create-customer.sh
@@ -10,16 +10,10 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=$TOKEN" \
   --data '{
-     "first name": "'"${FIRST_NAME}"'",
-     "last_name": "'"${LAST_NAME}"'",
-     "team": "'"${TEAM}"'",
-     "position": "'"${POSITION}"'",
-     "buckets": "'"${BUCKETS}"'",
-     "rpg": "'"${RPG}"'",
-     "apg": "'"${APG}"'",
-     "sponsors": "'"${SPONSORS}"'",
-     "shoes": "'"${SHOES}"'",
-     "catchphrase": "'"${CATCHPHRASE}"'"
+     "name": "'"${NAME}"'",
+     "email": "'"${EMAIL}"'",
+     "phone": "'"${PHONE}"'",
+
    }
  }'
 echo
