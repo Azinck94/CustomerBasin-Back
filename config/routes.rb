7bbd2f3c7436bcb02f3e5a62e5b08b4fcceba1ee
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-25 11:44:34 -0400

    addedresources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   resources :customers
+  resources :posts
   resources :examples, except: [:new, :edit]
+  get '/user-ballers/:id' => 'users#userballers'
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+  resources :customers, only: [:index, :show, :destroy, :create, :update]
 end
