591e105553c645a290cd03dbd390032f75aa7c63
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-31 17:55:44 -0400

    Adds has many relationship to the customer serializer
    added has_many to customer_serializer.rb

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,8 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone
+  attributes :id, :name, :email, :phone, :meetings
   has_one :user
+
+  def meetings
+    object.meetings.pluck(:id)
+  end
 end
